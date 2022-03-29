b27cceb5cf95b0ba23102fa639c56f01f9beff85
Harman Goei <hgoei@opentable.com>
2022-03-29 16:36:56 -0700

    Add an arbitrary wait

--- components/profile/Profile.server.js
+++ components/profile/Profile.server.js
@@ -1,9 +1,14 @@
 import fetch from "node-fetch";
 import { useData } from "../../libs/use-fetch";
 
+function wait(time) {
+  return new Promise((resolve, reject) => setTimeout(resolve, time));
+}
+
 export async function getData() {
   const res = await fetch(`https://swapi.dev/api/people/1`);
   const json = await res.json();
+  await wait(10000);
   return json;
 }
 
